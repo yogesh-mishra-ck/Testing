c3a8e6c70380d3faa95d6452c7fb3242a60dedee
yogesh-mishra-ck <yogeshmishra24x@gmail.com.com>
2025-11-03 15:39:35 +0530

    Week-1 Assignments

---
+++ test.js
@@ -0,0 +1,13 @@
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     printPerson(){
+//         console.log(`Name: ${this.name} and age is ${this.age}`);
+//     }
+// }
+// let a = new Person("Yogesh",21);
+// a.printPerson();
+
